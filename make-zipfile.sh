b1e7e5721655e9329e8df7b44e85951842496d21
luoxiangyong <solo_lxy@126.com>
2021-05-14 12:17:41 +0800

    添加了自动自作QGIS插件的zip文件，方便上传

---
+++ make-zipfile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+version=`grep version= metadata.txt | awk -F"=" '{print $2}'`
+echo The sprp VERSION:${version}
+cd ..
+zip -r  qgis-plugins/qgissprp-${version}.zip  qgissprp/ -x "*/.git/*"  -x "*/.git" -x "*/__pycache__/*" -x "*/.vscode/*"
+cd qgissprp
